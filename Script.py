3eac82079e1e47be6a2bc4e12464f75fd0076ced
JINTO N SEBASTIAN <65418412+jinto8301@users.noreply.github.com>
2022-03-20 14:44:22 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,10 +1,13 @@
 class script(object):
     START_TXT = """𝙷𝙴𝙻𝙾 {},
-𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href=https://t.me/{}>{}</a>, 𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝙼𝙾𝚅𝙸𝙴𝚂, 𝙹𝚄𝚂𝚃 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 𝙰𝙽𝙳 𝙴𝙽𝙹𝙾𝚈 😍"""
+𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href=https://t.me/{}>{}</a>, 𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝙼𝙾𝚅𝙸𝙴𝚂, 𝙹𝚄𝚂𝚃 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 𝙰𝙽𝙳 𝙴𝙽𝙹𝙾𝚈 😍
+
+𝙷𝙴𝙻𝙿->𝙷𝙾W 𝚃𝙾 𝚄𝚂𝙴 𝙼𝙴
+"""
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
     ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
-✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/TeamEvamaria>Team Eva Maria</a>
+✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/JNS_BOTS>❤️‍🔥ＪƝ⟆ ᗷ〇Ƭ⟆❤️‍🔥</a>
 ✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
 ✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
 ✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
@@ -12,6 +15,21 @@ class script(object):
 ✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
     
     JNS_TEAM = """Other movie bots from JNS FAMILY🔥
+    """
+    JNS_INLINE_HELP = """HOW TO USE ME ⁉️❔‼️
+    All JNS movie and series searching bots can be use by following methods
+    
+    @USERNAME_OF_BOT<space>movie/series name
+    
+    👇🏻JNS MAJOR 4 BOT'S USERNAMES👇🏻
+    <code>@JNS_SERIES_BOT</code>
+    <code>@JNSMOVIE_BOT</code> 
+    <code>@JNS_MOVIE_BOT</code> ✳️
+    <code>@Fc_media_search1_bot</code> ✳️
+    
+    ✳️just send movie name to bot(this featues only on ✳️ mentioned bots) 
+    
+    ‼️ CHECK SPELLING 
     """
     
     AUTOFILTER_TXT = """Help: <b>Auto Filter</b>
